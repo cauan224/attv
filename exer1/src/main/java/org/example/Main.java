eaa88aaec90f266ff326f85adb2f57b450d815f8
aluno.den <cauanbarbosa575@gmail.com>
2025-08-19 11:23:38 -0300

    add atividade

--- exer1/src/main/java/org/example/Main.java
+++ exer1/src/main/java/org/example/Main.java
@@ -2,11 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        cliente cliente1 =new cliente("João", 30, new org.example.exercio3.Endereco("Rua A", "123", "São Paulo"));
 
-        cliente c1 = new cliente("Maria", 25);
-
-
-        System.out.println("Nome: " + c1.getNome());
-        System.out.println("Idade: " + c1.getIdade());
+        System.out.println(cliente1.toString());
     }
 }
--- exer1/src/main/java/org/example/cliente.java
+++ exer1/src/main/java/org/example/cliente.java
@@ -1,16 +1,12 @@
 package org.example;
 
+import org.example.exercio3.Endereco;
+
 public class cliente {
-    private  String nome;
+    private String nome;
     private int idade;
+    private Endereco endereco;
 
-    // Construtor.
-    public cliente(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    //metodos da classe
     public String getNome() {
         return nome;
     }
@@ -19,7 +15,6 @@ public class cliente {
         this.nome = nome;
     }
 
-
     public int getIdade() {
         return idade;
     }
@@ -27,4 +22,31 @@ public class cliente {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public cliente(String nome, int idade, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "cliente{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
+
+
